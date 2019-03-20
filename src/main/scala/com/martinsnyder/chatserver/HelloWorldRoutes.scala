d0fc18ca6e1f085023c5d86b3f534b83a934edc6
Martin Snyder <github@martinsnyder.net>
2019-03-20 14:59:17 -0400

    Implement simple broadcast server

--- src/main/scala/com/martinsnyder/chatserver/HelloWorldRoutes.scala
+++ src/main/scala/com/martinsnyder/chatserver/HelloWorldRoutes.scala
@@ -3,19 +3,27 @@ package com.martinsnyder.chatserver
 import java.io.File
 
 import cats.effect.{ContextShift, Sync}
-import io.circe.Json
+import fs2.Stream
+import fs2.Pipe
+import fs2.concurrent.Topic
 import org.http4s.{HttpRoutes, StaticFile}
-import org.http4s.circe._
 import org.http4s.dsl.Http4sDsl
+import org.http4s.server.websocket.WebSocketBuilder
+import org.http4s.websocket.WebSocketFrame
+import org.http4s.websocket.WebSocketFrame.Text
+
 import scala.concurrent.ExecutionContext.global
 
-class HelloWorldRoutes[F[_]: Sync: ContextShift] extends Http4sDsl[F] {
+class HelloWorldRoutes[F[_]: Sync: ContextShift](topic: Topic[F, String]) extends Http4sDsl[F] {
   val routes: HttpRoutes[F] =
     HttpRoutes.of[F] {
       case request @ GET -> Root  => StaticFile.fromFile(new File("static/index.html"), global, Some(request)).getOrElseF(NotFound())
       case request @ GET -> Root / "chat.js"  => StaticFile.fromFile(new File("static/chat.js"), global, Some(request)).getOrElseF(NotFound())
 
-      case GET -> Root / "hello" / name =>
-        Ok(Json.obj("message" -> Json.fromString(s"Hello, ${name}")))
+      case GET -> Root / "ws" / userName =>
+        val toClient: Stream[F, WebSocketFrame.Text] = topic.subscribe(1000).map(Text(_))
+        val fromClient: Pipe[F, WebSocketFrame, Unit] = wsf => wsf.map({case Text(text, _) => text}).through(topic.publish)
+
+        WebSocketBuilder[F].build(toClient, fromClient)
     }
 }
--- src/main/scala/com/martinsnyder/chatserver/HelloWorldServer.scala
+++ src/main/scala/com/martinsnyder/chatserver/HelloWorldServer.scala
@@ -2,6 +2,7 @@ package com.martinsnyder.chatserver
 
 import cats.effect._
 import cats.implicits._
+import fs2.concurrent.Topic
 import org.http4s.HttpRoutes
 import org.http4s.server.Router
 import org.http4s.server.blaze.BlazeServerBuilder
@@ -9,17 +10,21 @@ import org.http4s.syntax.kleisli._
 
 object HelloWorldServer extends IOApp {
   def run(args: List[String]): IO[ExitCode] =
-    ServerStream.stream[IO].compile.drain.as(ExitCode.Success)
+    for (
+      topic <- Topic[IO, String]("initialized");
+      exitCode <- ServerStream.stream[IO](topic).compile.drain.as(ExitCode.Success)
+    ) yield exitCode
 }
 
 object ServerStream {
-  def helloWorldRoutes[F[_]: Effect: ContextShift]: HttpRoutes[F] = new HelloWorldRoutes[F].routes
+  def helloWorldRoutes[F[_]: Effect: ContextShift](topic: Topic[F, String]): HttpRoutes[F] =
+    new HelloWorldRoutes[F](topic).routes
 
-  def stream[F[_]: ConcurrentEffect: Timer: ContextShift]: fs2.Stream[F, ExitCode]=
+  def stream[F[_]: ConcurrentEffect: Timer: ContextShift](topic: Topic[F, String]): fs2.Stream[F, ExitCode]=
     BlazeServerBuilder[F]
       .bindHttp(8080, "0.0.0.0")
       .withHttpApp(Router(
-        "/" -> helloWorldRoutes
+        "/" -> helloWorldRoutes(topic)
       ).orNotFound)
       .serve
 }
