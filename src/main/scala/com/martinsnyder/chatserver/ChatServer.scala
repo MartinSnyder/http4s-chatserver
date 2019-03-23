b07955e2a87a3ace5de32dfef22e8234b00c540f
Martin Snyder <github@martinsnyder.net>
2019-03-23 11:16:41 -0400

    Add Environment/Command Line HTTP port configuration

--- src/main/scala/com/martinsnyder/chatserver/ChatServer.scala
+++ src/main/scala/com/martinsnyder/chatserver/ChatServer.scala
@@ -1,5 +1,6 @@
 package com.martinsnyder.chatserver
 
+import scala.util.Try
 import cats.effect._
 import cats.implicits._
 import fs2.Stream
@@ -13,14 +14,21 @@ import org.http4s.syntax.kleisli._
  * Top-level executor for our application
  */
 object HelloWorldServer extends IOApp {
-  def run(args: List[String]): IO[ExitCode] =
+  def run(args: List[String]): IO[ExitCode] = {
+    // Read a potential tcp port from the command line
+    val httpPort = args
+      .headOption
+      .orElse(sys.env.get("PORT"))
+      .flatMap(s => Try(s.toInt).toOption)
+      .getOrElse(8080)
+
     for (
       // Synchronization objects must be created at the top-level so they can be shared across the application
       queue <- Queue.unbounded[IO, InputMessage];
       topic <- Topic[IO, OutputMessage](SendToUsers(Set.empty, ""));
       exitCode <- {
         // Stream for HTTP requests
-        val httpStream = ServerStream.stream[IO](queue, topic)
+        val httpStream = ServerStream.stream[IO](httpPort, queue, topic)
 
         // Stream to process items from the queue and publish the results to the topic
         // Note mapAccumulate below which performs our state manipulation
@@ -37,6 +45,7 @@ object HelloWorldServer extends IOApp {
       }
 
     ) yield exitCode
+  }
 }
 
 object ServerStream {
@@ -44,9 +53,9 @@ object ServerStream {
     new ChatRoutes[F](queue, topic).routes
 
   // Builds a stream for HTTP events processed by our router
-  def stream[F[_]: ConcurrentEffect: Timer: ContextShift](queue: Queue[F, InputMessage], topic: Topic[F, OutputMessage]): fs2.Stream[F, ExitCode]=
+  def stream[F[_]: ConcurrentEffect: Timer: ContextShift](port: Int, queue: Queue[F, InputMessage], topic: Topic[F, OutputMessage]): fs2.Stream[F, ExitCode]=
     BlazeServerBuilder[F]
-      .bindHttp(8080, "0.0.0.0")
+      .bindHttp(port, "0.0.0.0")
       .withHttpApp(Router(
         "/" -> helloWorldRoutes(queue, topic)
       ).orNotFound)
